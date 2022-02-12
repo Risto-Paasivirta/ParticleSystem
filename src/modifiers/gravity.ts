861d41fc5a96400f6591accc3df27579ba05247d
Nipatsku <niilo.keinanen@hotmail.com>
2022-02-12 11:04:21 +0200

    Disable eslint rule on unused variable.

--- src/modifiers/gravity.ts
+++ src/modifiers/gravity.ts
@@ -4,6 +4,7 @@ export class Gravity extends Module {
     gravity = 2;
     modifier = 0.1;
 
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     update(dt: number): void {
         this.parentSystem.particles.forEach((particle) => {
             particle.velocity.y += this.modifier * this.gravity;
