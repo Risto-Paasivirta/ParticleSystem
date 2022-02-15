86deb11f1c905f5c60e28d20f345e8a4213ad1cd
Nipatsku <niilo.keinanen@hotmail.com>
2022-02-15 22:35:45 +0200

    Add random angle velocity modifier

--- src/util.ts
+++ src/util.ts
@@ -2,3 +2,10 @@ export const lerp = (x: number, y: number, a: number) => x * (1 - a) + y * a;
 export const clamp = (a: number, min = 0, max = 1) => Math.min(max, Math.max(min, a));
 export const invlerp = (x: number, y: number, a: number) => clamp((a - x) / (y - x));
 export const range = (x1: number, y1: number, x2: number, y2: number, a: number) => lerp(x2, y2, invlerp(x1, y1, a));
+/**
+ * Get random number between range [min, max].
+ * @param min
+ * @param max
+ * @returns
+ */
+export const randomInRange = (min: number, max: number): number => min + Math.random() * (max - min);
