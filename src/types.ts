5ea74838a1020a9191c2447411e6d225437077de
Nipatsku <niilo.keinanen@hotmail.com>
2022-02-16 19:32:21 +0200

    Add note in main Color type

--- src/types.ts
+++ src/types.ts
@@ -2,5 +2,11 @@
 export type Position = { x: number; y: number };
 export type Velocity = { x: number; y: number };
 export type Range = { min: number; max: number };
+
+/**
+ * Type definition for a color.
+ *
+ * Colors are stored as RGBA data structures with values in range [0, 1].
+ */
 export type Color = { r: number; g: number; b: number; a: number };
 export type Shape = { x: number; y: number };
