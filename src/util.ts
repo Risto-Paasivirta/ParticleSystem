8c10d86a62dd65740aec67e6065fc75a6e4b3fcf
Nipatsku <niilo.keinanen@hotmail.com>
2022-02-16 15:31:15 +0200

    Remove unused randomIntFromRange

--- src/util.ts
+++ src/util.ts
@@ -2,4 +2,3 @@ export const lerp = (x: number, y: number, a: number) => x * (1 - a) + y * a;
 export const clamp = (a: number, min = 0, max = 1) => Math.min(max, Math.max(min, a));
 export const invlerp = (x: number, y: number, a: number) => clamp((a - x) / (y - x));
 export const range = (x1: number, y1: number, x2: number, y2: number, a: number) => lerp(x2, y2, invlerp(x1, y1, a));
-export const randomIntFromRange = (min: number, max: number) => Math.floor(Math.random() * (max - min + 1) + min);
