caca0ef2ba7370d18abac6757a3978be996b6739
Nipatsku <niilo.keinanen@hotmail.com>
2022-02-16 15:31:02 +0200

    Clarify property names, add descriptions, remove random from default configurations

--- src/generators/circleExteriorGenerator.ts
+++ src/generators/circleExteriorGenerator.ts
@@ -1,7 +1,6 @@
 import { Position } from "../types";
 import { Module } from "../module";
 import { Particle } from "../particle";
-import { randomIntFromRange } from "../util";
 
 // NOTE: Ideally we would have a class for `CircleGenerator`, where user could configure whether to generate particles inside the circle or along the exterior.
 // `generator.onlyExterior = true` or something like this.
@@ -13,12 +12,28 @@ import { randomIntFromRange } from "../util";
  */
 export class CircleExteriorGenerator extends Module {
     interval = 0.1;
-    position: Position = { x: 0, y: 0 };
-    radians = Math.random() * Math.PI * 2;
-    velocity = 0.05;
-    distanceFromCenter = randomIntFromRange(50, 120);
-
     private _timer = 0;
+
+    /**
+     * Center location of the circle.
+     */
+    center: Position = { x: 0, y: 0 };
+    /**
+     * Radius of the circle as pixels.
+     */
+    radius = 50;
+    /**
+     * The angle at which the next particle will be generated at.
+     *
+     * Unit is in _radians_.
+     */
+    nextParticleAngle = 0;
+    /**
+     * The angle that is incremented between each generated particle.
+     *
+     * Unit is in _radians_.
+     */
+    angleStep = 0.5;
 
     update(dt: number): void {
         //avoid infinite loop
@@ -26,19 +41,19 @@ export class CircleExteriorGenerator extends Module {
             return;
         }
 
-        this.radians += this.velocity;
         this._timer += dt;
 
         while (this._timer >= this.interval) {
             this._timer -= this.interval;
-            this.createParticle(this.radians);
+            this.createParticle();
         }
     }
 
-    createParticle(radians: number) {
+    createParticle() {
         const particle = new Particle();
-        particle.position.x = this.position.x + Math.cos(radians) * this.distanceFromCenter;
-        particle.position.y = this.position.y + Math.sin(radians) * this.distanceFromCenter;
+        particle.position.x = this.center.x + Math.cos(this.nextParticleAngle) * this.radius;
+        particle.position.y = this.center.y + Math.sin(this.nextParticleAngle) * this.radius;
+        this.nextParticleAngle += this.angleStep;
 
         this.parentSystem.addParticle(particle);
     }
