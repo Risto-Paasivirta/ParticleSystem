7a9e00f5bed884ab2e5586f295de86eb4926240b
Nipatsku <niilo.keinanen@hotmail.com>
2022-02-20 18:32:55 +0200

    Remove swc along with some scripts

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,13 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 
 const path = require("path");
-const fs = require("fs");
-
-const merge = require("webpack-merge").merge;
-
-// plugins
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const CopyPlugin = require("copy-webpack-plugin");
 const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const fs = require("fs");
+// const ESLintPlugin = require("eslint-webpack-plugin");
+// const webpack = require("webpack");
 
 const getFiles = (src, ext, files = []) => {
     const list = fs.readdirSync(src);
@@ -24,90 +22,94 @@ const getFiles = (src, ext, files = []) => {
     return files;
 };
 
-module.exports = (env) => {
-    const appsFiles = getFiles("./apps/", ".ts");
-    const entry = appsFiles.reduce((obj, [filePath, name]) => {
-        obj[name] = `./${filePath}`;
-        return obj;
-    }, {});
+const appsFiles = getFiles("./apps/", ".ts");
+const entry = appsFiles.reduce((obj, [filePath, name]) => {
+    obj[name] = `./${filePath}`;
+    return obj;
+}, {});
 
-    const config = {
-        entry,
-
-        resolve: {
-            extensions: [".ts", ".tsx", ".js", ".json"],
-            plugins: [new TsconfigPathsPlugin()],
-        },
-
-        module: {
-            rules: [
+module.exports = {
+    entry,
+    resolve: {
+        extensions: [".ts", "..."],
+        plugins: [new TsconfigPathsPlugin()],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/i,
+                use: ["ts-loader"],
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.css$/i,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                    },
+                    "css-loader",
+                ],
+            },
+        ],
+    },
+    mode: "development",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "[name].js",
+    },
+    plugins: [
+        // Generate html file for each file inside apps/ folder.
+        ...appsFiles.map(([src, name]) => {
+            const srcTo = src.split("/");
+            return new HtmlWebpackPlugin({
+                title: name,
+                chunks: [name],
+                filename:
+                    // check if path includes nested folders
+                    srcTo.length > 2
+                        ? // construct path to html document
+                          `${srcTo.slice(1, srcTo.length - 1).join("/")}/${name}.html`
+                        : `${name}.html`,
+            });
+        }),
+        // Generate index.html page for development webpack build.
+        // This is just simple HTML that contains a link to every file inside apps/ folder.
+        new HtmlWebpackPlugin({
+            chunks: [],
+            title: "Index",
+            templateParameters: {
+                apps: appsFiles.map(([src, name]) => {
+                    const srcTo = src.split("/");
+                    return srcTo.length > 2
+                        ? `${srcTo.slice(1, srcTo.length - 1).join("/")}/${name}.html`
+                        : `${name}.html`;
+                }),
+            },
+            filename: "index.html",
+            template: "./index-template.html",
+        }),
+        // Copy assets into webpack build.
+        new CopyPlugin({
+            patterns: [
                 {
-                    test: /\.css$/i,
-                    use: [
-                        {
-                            loader: MiniCssExtractPlugin.loader,
-                        },
-                        "css-loader",
-                    ],
+                    from: "assets/**",
                 },
             ],
-        },
-        optimization: {
-            splitChunks: {
-                cacheGroups: {
-                    defaultVendor: {
-                        test: /[\\/]node_modules[\\/]/,
-                        idHint: "vendor",
-                        chunks: "all",
-                    },
+        }),
+        new MiniCssExtractPlugin({
+            filename: "[name].css",
+        }),
+    ],
+    devtool: "inline-source-map",
+    optimization: {
+        splitChunks: {
+            cacheGroups: {
+                defaultVendor: {
+                    test: /[\\/]node_modules[\\/]/,
+                    idHint: "vendor",
+                    chunks: "all",
                 },
             },
         },
-
-        plugins: [
-            // Generate html file for each file inside apps/ folder.
-            ...appsFiles.map(([src, name]) => {
-                const srcTo = src.split("/");
-                return new HtmlWebpackPlugin({
-                    title: name,
-                    chunks: [name],
-                    filename:
-                        // check if path includes nested folders
-                        srcTo.length > 2
-                            ? // construct path to html document
-                              `${srcTo.slice(1, srcTo.length - 1).join("/")}/${name}.html`
-                            : `${name}.html`,
-                });
-            }),
-            // Generate index.html page for development webpack build.
-            // This is just simple HTML that contains a link to every file inside apps/ folder.
-            new HtmlWebpackPlugin({
-                chunks: [],
-                title: "Index",
-                templateParameters: {
-                    apps: appsFiles.map(([src, name]) => {
-                        const srcTo = src.split("/");
-                        return srcTo.length > 2
-                            ? `${srcTo.slice(1, srcTo.length - 1).join("/")}/${name}.html`
-                            : `${name}.html`;
-                    }),
-                },
-                filename: "index.html",
-                template: "./index-template.html",
-            }),
-            // Copy assets into webpack build.
-            new CopyPlugin({
-                patterns: [
-                    {
-                        from: "assets/**",
-                    },
-                ],
-            }),
-        ],
-    };
-    const envConfig = require(path.resolve(__dirname, `./webpack.${env.mode}.js`))(env);
-
-    const mergedConfig = merge(config, envConfig);
-
-    return mergedConfig;
+    },
 };
